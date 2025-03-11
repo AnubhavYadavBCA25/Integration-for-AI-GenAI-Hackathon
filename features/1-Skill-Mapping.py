bd740d44001b93d4cbf5de0e458e8d254f26ce70
Anubhav Yadav <ay6666@srmist.edu.in>
2025-03-11 11:57:03 +0530

    Modified model

--- features/1-Skill-Mapping.py
+++ features/1-Skill-Mapping.py
@@ -67,7 +67,7 @@ sys_intructions = {"""
 """}
 
 # Load Gemini Model
-model = genai.GenerativeModel(model_name="gemini-2.0-flash",
+model = genai.GenerativeModel(model_name="gemini-1.5-flash",
                               generation_config=generation_config,
                               safety_settings=safety_settings,
                               system_instruction=sys_intructions)
